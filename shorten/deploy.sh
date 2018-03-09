7bfd3c6ae2a31c2efc9e36cd9ce74c2899748464
tinrab <tin.rabzelj@gmail.com>
2018-03-09 14:43:58 +0100

    Remove role ID

--- shorten/deploy.sh
+++ shorten/deploy.sh
@@ -3,4 +3,4 @@ set GOOS=linux
 go build -o shorten main.go
 build-lambda-zip -o deployment.zip shorten
 
-aws lambda create-function --region us-east-1 --function-name ShortenFunction --zip-file fileb://./deployment.zip --runtime go1.x --tracing-config Mode=Active --role arn:aws:iam::766594786016:role/lambda_basic_execution --handler shorten
+aws lambda create-function --region us-east-1 --function-name ShortenFunction --zip-file fileb://./deployment.zip --runtime go1.x --tracing-config Mode=Active --role $ROLE --handler shorten
